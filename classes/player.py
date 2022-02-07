ed5e30d9f41da05b11d11f3ad9e44723f23df04c
João Pedro <joaopedrorafael19@gmail.com>
2022-02-07 14:23:51 -0300

    level, tiles, setup, map start

---
+++ classes/player.py
@@ -0,0 +1,13 @@
+import pygame
+from settings import *
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = self.get_image('./assets/assets/eskimo/Item.png')
+        self.rect = self.image.get_rect(topleft=pos)
+
+    def get_image(self, image: str) -> pygame.Surface:
+        return pygame.transform.scale(pygame.image.load(image).convert_alpha(), (Settings().TILE_SIZE, Settings().TILE_SIZE))
---
+++ classes/tiles.py
@@ -0,0 +1,14 @@
+import pygame
+
+from settings import *
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = self.get_image(
+            './assets/assets/nature/rock_snow.png')
+        self.rect = self.image.get_rect(topleft=pos)
+
+    def get_image(self, image: str) -> pygame.Surface:
+        return pygame.transform.scale(pygame.image.load(image).convert_alpha(), (Settings().TILE_SIZE, Settings().TILE_SIZE))
